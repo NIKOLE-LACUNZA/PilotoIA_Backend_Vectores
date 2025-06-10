30671676e10e2c450e9db07ebd328a598dbb4670
NIKOLE-LACUNZA <lacunzanikole@gmail.com>
2025-06-10 11:26:27 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,41 +2,21 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using DotNetEnv;
+using DotNetEnv; 
 using Proyecto_IA.Models;
-using Proyecto_IA.Data;
-using Microsoft.AspNetCore.Http;
-using System.IO;
+using Proyecto_IA.Data;  // Para DocumentoStore
 using System.Threading.Tasks;
 
-// Inicializar builder
 var builder = WebApplication.CreateBuilder(args);
 
-// Cargar variables de entorno
 Env.Load();
 
-// Configurar claves desde variables de entorno
 builder.Configuration["AzureBlobStorage:ConnectionString"] =
     Environment.GetEnvironmentVariable("AZURE_STORAGE");
 
 builder.Configuration["OpenAI:ApiKey"] =
     Environment.GetEnvironmentVariable("OPENAI_API_KEY");
 
-// Permitir request de hasta 1 GB
-const long oneGigabyte = 1_073_741_824;
-
-builder.WebHost.ConfigureKestrel(serverOptions =>
-{
-    serverOptions.Limits.MaxRequestBodySize = oneGigabyte;
-});
-
-builder.Services.Configure<IISServerOptions>(options =>
-{
-    options.MaxRequestBodySize = oneGigabyte;
-});
-
-// Servicios
 builder.Services.AddControllers();
 builder.Services.AddSingleton<AzureBlobService>();
 builder.Services.AddSingleton<PdfReaderService>();
@@ -48,7 +28,6 @@ var app = builder.Build();
 app.UseAuthorization();
 app.MapControllers();
 
-// Cargar documentos iniciales desde Azure
 await CargarDocumentosDesdeAzureAsync(app.Services);
 
 app.Run();
@@ -59,7 +38,7 @@ async Task CargarDocumentosDesdeAzureAsync(IServiceProvider services)
     var pdfReader = services.GetRequiredService<PdfReaderService>();
 
     var archivos = await blobService.ListarArchivosAsync();
-
+    
     foreach (var archivo in archivos)
     {
         string nombreArchivo = archivo.Nombre;
@@ -68,9 +47,10 @@ async Task CargarDocumentosDesdeAzureAsync(IServiceProvider services)
             continue;
 
         var blobClient = blobService.ContainerClient.GetBlobClient(nombreArchivo);
-        var downloadResponse = await blobClient.DownloadAsync();
 
+        var downloadResponse = await blobClient.DownloadAsync();
         using var originalStream = downloadResponse.Value.Content;
+
         using var memoryStream = new MemoryStream();
         await originalStream.CopyToAsync(memoryStream);
         memoryStream.Position = 0;
